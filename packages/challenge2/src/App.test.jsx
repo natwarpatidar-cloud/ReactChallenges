1fae4f1cbbcb7311f9b22bb4e21228d406c7f493
NatwarPatidar <natwar.patidar@gammaedge.io>
2025-10-29 16:13:12 +0530

    add test for challenge2: stopwatch/timer

---
+++ packages/challenge2/src/App.test.jsx
@@ -0,0 +1,63 @@
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import App from '../src/App';
+
+jest.useFakeTimers();
+
+describe('Timer App', () => {
+  it('renders initial timer values correctly', () => {
+    render(<App />);
+    expect(screen.getByText(/0 mins/i)).toBeInTheDocument();
+    expect(screen.getByText(/56 secs/i)).toBeInTheDocument();
+  });
+
+  it('starts and increments timer when Start is clicked', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('Start'));
+
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+
+    expect(screen.getByText(/58 secs/i)).toBeInTheDocument();
+  });
+
+  it('stops timer when Stop is clicked', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('Start'));
+
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+
+    fireEvent.click(screen.getByText('Stop'));
+
+    expect(screen.getByText(/58 secs/i)).toBeInTheDocument();
+  });
+
+  it('resets timer when Reset is clicked', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('Start'));
+
+    act(() => {
+      jest.advanceTimersByTime(2000);
+    });
+    expect(screen.getByText(/58 secs/i)).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Reset'));
+
+    expect(screen.getByText(/0 mins/i)).toBeInTheDocument();
+    expect(screen.getByText(/0 secs/i)).toBeInTheDocument();
+  });
+
+  it('increments minutes correctly after 60 seconds', () => {
+    render(<App />);
+    fireEvent.click(screen.getByText('Start'));
+
+    act(() => {
+      jest.advanceTimersByTime(4000); 
+    });
+
+    expect(screen.getByText(/1 mins/i)).toBeInTheDocument();
+    expect(screen.getByText(/0 secs/i)).toBeInTheDocument();
+  });
+});
